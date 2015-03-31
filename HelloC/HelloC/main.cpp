ecfb5d7b42de2f2ebcc017ab295ce774bb5a0507
chaitanchun <tanchun@Tanchuns-MacBook-Pro.local>
2015-04-01 00:49:23 +0800

    hello2

---
+++ HelloC/HelloC/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  HelloC
+//
+//  Created by Tanchun Chai on 1/4/15.
+//  Copyright (c) 2015 Tanchun Chai. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(int arg, const char * argv[]) {
+    // insert code here...
+    vector<int> arr(10);
+    
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- HelloWorld/HelloWorld/main.swift
+++ HelloWorld/HelloWorld/main.swift
@@ -5,6 +5,14 @@
 //  Created by Tanchun Chai on 1/4/15.
 //  Copyright (c) 2015 Tanchun Chai. All rights reserved.
 //
+var str = "sdkfj"
+println("sdkskldfj")
 
-println("Hello, World!")
+class MyClass {
+    var a: Int = 0
+    var b = "dskjs"
+}
 
+class MyChild: MyClass {
+    var c = 1.2
+}
